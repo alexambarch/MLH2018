a9615326ce29027ed5635e5d5232b14d95c518a3
Alexi <alexiamb@buffalo.edu>
2018-11-03 16:33:56 -0400

    update file so justin gets the latest version

---
+++ code/slicer/slicer.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+class Slicer(image_path):
+
+    PARKING_SPOTS = [(), (), (), (), (), (), (), (), (), (), (), (), (), ()]
+    LOT_IMAGE = Image.open(image_path)
+    subimage = 0
+
+    def slice(image, width, height):
+        Image.crop()
+
+
+
