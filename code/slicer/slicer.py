69676af28b67ce72d5833f11460b190c81dc8837
Alex Ambarchian <alexiamb@buffalo.edu>
2018-11-04 13:17:45 -0500

    Deleted stolen things, added some slicing code

--- code/slicer/slicer.py
+++ code/slicer/slicer.py
@@ -1,13 +1,15 @@
 from PIL import Image
 
-class Slicer(image_path):
-
-    PARKING_SPOTS = [(), (), (), (), (), (), (), (), (), (), (), (), (), ()]
-    LOT_IMAGE = Image.open(image_path)
-    subimage = 0
-
+class Slicer():
     def slice(image, width, height):
-        Image.crop()
+        image_path = "../../images"
+        PARKING_SPOTS = [(), (), (), (), (), (), (), (), (), (), (), (), (), ()]
+        LOT_IMAGE = Image.open(image_path)
+        subimage = 0
+        for i in range(0, 14):
+            a = LOT_IMAGE.crop(PARKING_SPOTS[i][1], PARKING_SPOTS[i][2]) # how to make a good box out of two pixels?
+            a.save(str(PARKING_SPOTS[i][0]) + '.png')
+
 
 
 
--- code/www/index.py
+++ code/www/index.py
@@ -1,5 +1,21 @@
 from flask import render_template
+import os
+from code.slicer import slicer
+from code.Detector import recognition
+from picamera import PiCamera # picamera or pycamera?
+
 
 class Index:
     def render_index(name=None):
-#        return render_template("index.html", name=name)
+        # the following will only be able to run on the raspberry pi
+        image_path = "../../images"
+        camera = PiCamera()
+        camera.capture(image_path, "image.png")
+
+        slicer.Slicer.slice(image_path)
+
+        for image_path in os.listdir(image_path):
+            print('Sending image' + image_path)
+            # TODO send each image to be detected
+
+        return render_template("index.html", name=name)
